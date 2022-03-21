637df45c74f7ede495e1f89a1bfafea4ee77af39
SJY-Windows <sejin.yoon@student.adelaide.edu.au>
2022-03-22 01:56:46 +1030

    Added Bureaucrat.h

---
+++ Bureaucrat.h
@@ -0,0 +1,14 @@
+// Bureaucrat.h file
+#include "Computer.h"
+
+#ifndef BUREAUCRAT_H_
+#define BUREAUCRAT_H_
+
+//  Bureaucrat Class
+class Bureaucrat : public Computer
+{
+    public:  
+    char Bureaucrat::makeMove();
+
+};
+#endif /* BUREAUCRAT_H_ */
